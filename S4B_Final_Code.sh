e1567420efd58a3b1389188904e04c04cb9e84e7
Emily M. Wilkins <emw0083@auburn.edu>
2022-04-23 10:54:46 -0500

    editing alignment function

--- S4B_Final_Code.sh
+++ S4B_Final_Code.sh
@@ -153,12 +153,29 @@ function INDEX_genome {
 
 
 #------------------------Step 5 - Align FASTQ Files to the Indexed Genome ------------------------------------------------
+"""
+Talk about aligning reads and outout files .......
+http://bowtie-bio.sourceforge.net/bowtie2/manual.shtml#paired-inputs for 
+"""
+
+function Aligning_Reads {
+    """
+    define function
+    
+    
+    """
+    source /opt/asn/etc/asn-bash-profiles-special/modules.sh
+    module load tophat/2.1.2
+    module load bowtie2/2.2.9
+
+    cd trimmed_reads/Case
+    #needed to hardcode the file names in order to get it to run
+    bowtie2 -x mouse -1 4040-KH-14.4040-KH-14_0_filtered_R1_val_1.fq -2 4040-KH-14.4040-KH-14_0_filtered_R2_val_2.fq  -S aligned_genome_sequences14.sam
+
+    cd ../Control
+    bowtie2 -x mouse
 
 
-
-
-
-
-
+}
 
 
